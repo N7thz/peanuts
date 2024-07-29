2e462aac70ed84d1b13055ff80381f8ddbb3bbdb
N7thz <nathanferreiradev@gmail.com>
2024-07-29 18:41:06 -0300

    fix: alteração na chamada dos links

--- src/app/api/links/[id]/route.ts
+++ src/app/api/links/[id]/route.ts
@@ -1,17 +1,8 @@
 import { ContextProps } from "@/@types"
 import { prisma } from "@/lib/prisma"
-import { validateToken } from "@/lib/validate-token"
-import { NextRequest, NextResponse } from "next/server"
+import { NextResponse } from "next/server"
 
-export async function GET(request: NextRequest, context: ContextProps) {
-
-    const decoded = validateToken(request)
-
-    if (!decoded) return NextResponse.json(
-        "unauthorized", {
-        status: 401,
-        statusText: "Error in request"
-    })
+export async function GET(context: ContextProps) {
 
     const { params: { id } } = context
 
@@ -20,8 +11,6 @@ export async function GET(request: NextRequest, context: ContextProps) {
             id
         }
     })
-
-    console.log(link)
 
     if (link) return NextResponse.json(
         link,
